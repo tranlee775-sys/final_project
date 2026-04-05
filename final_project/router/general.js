791347f82355144e6cd883d5974a340d2fc079ae
tranlee775-sys <tranlee775@gmail.com>
2026-04-05 18:19:04 +0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,16 +11,15 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  // Trả về danh sách sách đã được định nghĩa ở biến 'books' (dòng 2)
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  return res.status(200).send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
